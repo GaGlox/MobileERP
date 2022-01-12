978456c2973799c24479a0d30a0749ab4b863a2c
Karl Dilumueni <cdilumueni@gmail.com>
2022-01-12 17:13:50 +0100

    Glox has committed stuffs

---
+++ app/src/main/java/com/driveup/erp/HomeActivity.java
@@ -0,0 +1,178 @@
+package com.driveup.erp;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.driveup.erp.ui.dashboard.DashboardFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    public static FragmentManager fragmentManager;
+    private TextView username, user_role;
+    public static HomeActivity HomeContextActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(HomeActivity.this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+
+        username = (TextView) headerView.findViewById(R.id.user_name);
+        user_role = (TextView) headerView.findViewById(R.id.user_role);
+
+        String nom = getIntent().getStringExtra("name");
+        String role = getIntent().getStringExtra("role");
+
+        username.setText(nom);
+        user_role.setText(role);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.nav_dashboard:
+                        fragment = new DashboardFragment();
+                        toolbar.setTitle("Tableau de board");
+                        setMyFragment(fragment);
+                        break;
+                    case R.id.nav_command:
+                        //fragment = new CommandFragment();
+                        toolbar.setTitle("Commande");
+                        //setMyFragment(fragment);
+                        break;
+                    case R.id.nav_provisioning:
+                        //fragment = new ProvisioningFragment();
+                        toolbar.setTitle("Approvisionnement");
+                        //setMyFragment(fragment);
+                        break;
+                    case R.id.nav_production:
+                        //fragment = new ProductionFragment();
+                        toolbar.setTitle("Production");
+                        //setMyFragment(fragment);
+                        break;
+                    case R.id.nav_catalog:
+                        //fragment = new CatalogFragment();
+                        toolbar.setTitle("Catalogue");
+                        //setMyFragment(fragment);
+                        break;
+                    case R.id.nav_about:
+                        //startActivity(new Intent(HomeActivity.this, AboutActivity.class));
+                        break;
+                    /*case R.id.nav_share:
+                        Intent intent = new Intent(Intent.ACTION_SEND);
+                        intent.setType("text/plain");
+                        intent.putExtra(Intent.EXTRA_SUBJECT, "Télécharger l'application");
+                        intent.putExtra(Intent.EXTRA_TEXT, "https://mydom-kinsafety.web.app/" );
+                        startActivity(Intent.createChooser(intent, "Partager via..."));
+                        break;*/
+                    case R.id.nav_logout:
+                        //startActivity(new Intent(HomeActivity.this, InfoActivity.class));
+                        break;
+                    default:
+                        fragment = new DashboardFragment();
+                        setMyFragment(fragment);
+                }
+
+                // Close the navigation drawer
+                drawer.closeDrawers();
+
+                return true;
+            }
+        });
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.nav_frame, new DashboardFragment()).commit();
+
+        HomeActivity.HomeContextActivity = HomeActivity.this;
+    }
+
+    private void setMyFragment(Fragment fragment) {
+        //get current fragment manager
+        fragmentManager = getSupportFragmentManager();
+        //get fragment transaction
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //set new fragment in fragment_container (FrameLayout)
+        fragmentTransaction.replace(R.id.nav_frame, fragment, "CURRENT");
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.END)) {
+            drawer.closeDrawer(GravityCompat.END);
+        } else {
+            new AlertDialog.Builder(HomeActivity.this)
+                    .setTitle("")
+                    .setMessage("Quitter l'application ?")
+                    .setNegativeButton(R.string.confirm_no, null)
+                    .setPositiveButton(R.string.confirm_yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            HomeActivity.super.onBackPressed();
+                        }
+                    }).create().show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            //case R.id.action_fingerprint:
+            //    break;
+            default:
+                Toast.makeText(this, "Great", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/driveup/erp/LoginActivity.java
@@ -0,0 +1,64 @@
+package com.driveup.erp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText login_username, login_password;
+    private Button bt_cancel, bt_validate;
+    private String nom, mot_de_passe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Initialisation des récupérations de variables de l'interface Login
+        login_username = (EditText) findViewById(R.id.login_username);
+        login_password = (EditText)findViewById(R.id.login_password);
+
+        bt_cancel = (Button) findViewById(R.id.login_cancel_button);
+        bt_validate = (Button) findViewById(R.id.login_validate_button);
+
+        nom = login_username.getText().toString().trim();
+        mot_de_passe = login_password.getText().toString().trim();
+
+        // Evènement Clic sur bouton annuler
+        bt_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login_username.setText(null);
+                login_password.setText(null);
+            }
+        });
+
+        // Evènement Clic sur bouton connexion
+        bt_validate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tmp_username = login_username.getText().toString().trim();
+                String tmp_password = login_password.getText().toString().trim();
+
+                if (tmp_username.equals("ANNE") && tmp_password.equals("1")){
+                    // On prépare une Intention d'ouverture de la fenêtre Accueil
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    intent.putExtra("name", "MONKAMBULA ANNE");
+                    intent.putExtra("role", "Chef de projet");
+                    startActivity(intent); // On lance l'interface Accueil grâce à l'intention préparée
+                    finish();
+
+                    Toast.makeText(LoginActivity.this, "Bienvenu", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Nom d\'utilisateur ou mot de passe incorrect", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
